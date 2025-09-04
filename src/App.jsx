9022581eafee3869eb66a24b3d20e7892c9d1774
Tuğçe Kanbur <tugcekanbr@gmail.com>
2025-09-04 16:22:57 +0300

    Vite + Tailwind + Router v5 + RTK setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <h1 className="text-3xl font-bold text-blue-600 p-6">
+      Tailwind çalışıyor 🎉
+    </h1>
+  );
+}
+
+export default App
